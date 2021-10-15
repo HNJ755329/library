01a08a1459df7d83a18bf6214d19028f3797eadd
Yasuyuki Honjo <y.honjo.i@gmail.com>
2021-10-15 19:01:19 +0900

    delete ugly files and shape files (#11)

--- library/TrieTree.hpp
+++ library/TrieTree.hpp
@@ -1,32 +1,30 @@
 #include <iostream>
 #include <vector>
-using namespace std;
-
 template <int char_size, int base>
 struct Trie
 {
     struct Node
-    {                           // 頂点を
-        vector<int> next;       // 子の頂点番号を格納。存在しなければ-1
-        vector<int> accept;     // 末端がこの頂点になる文字列の str_id を保存
-        int c;                  // base からの間隔をint型で表現したもの
-        int common;             // いくつの文字列がこの頂点を共有しているか
-        vector<long long int> common_char; //文字cを含む子孫の数
+    {                                           // 頂点を
+        std::vector<int> next;                  // 子の頂点番号を格納。存在しなければ-1
+        std::vector<int> accept;                // 末端がこの頂点になる文字列の str_id を保存
+        int c;                                  // base からの間隔をint型で表現したもの
+        int common;                             // いくつの文字列がこの頂点を共有しているか
+        std::vector<long long int> common_char; //文字cを含む子孫の数
         Node(int c_) : c(c_), common(0)
         {
             next.assign(char_size, -1);
             common_char.assign(char_size, 0);
         }
     };
-    vector<Node> nodes; // trie 木本体
-    int root;           // 根
+    std::vector<Node> nodes; // trie 木本体
+    int root;                // 根
     Trie() : root(0)
     {
         // 初期化。はじめは根しか無い
         nodes.push_back(Node(root));
     }
 
-    void insert(const string &word, int word_id)
+    void insert(const std::string &word, int word_id)
     { // 単語:str と 単語の番号: str_id
         int node_id = 0;
         for (int i = 0; i < (int)word.size(); i++)
@@ -44,10 +42,10 @@ struct Trie
         ++nodes[node_id].common;
         nodes[node_id].accept.push_back(word_id); // 単語の終端なので、頂点に番号を入れておく
     }
-    void insert(const string &word) { insert(word, nodes[0].common); }
+    void insert(const std::string &word) { insert(word, nodes[0].common); }
 
     // 単語とprefixの検索. indexを返す
-    int search(const string &word)
+    int search(const std::string &word)
     {
         int node_id = 0;
         for (int i = 0; i < (int)word.size(); i++)
@@ -86,8 +84,3 @@ struct Trie
         }
     }
 };
-
-int main()
-{
-    return 0;
-}
